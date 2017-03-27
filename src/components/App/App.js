b5de64beeb2248132ebd66862ce2af2e89b011ff
Nate Arnold <nate.arnold@intouchsol.com>
2017-03-27 10:12:30 -0500

    first commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
 
 import BigCalendar from 'react-big-calendar';
@@ -6,39 +6,83 @@ BigCalendar.setLocalizer(
   BigCalendar.momentLocalizer(moment)
 );
 
-import './App.css';
 import '../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
+import './App.css';
 
-import { getEvents } from '../../events';
+import { getEvents, calendarUrls } from '../../events';
 
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
+      filters: [],
       events: []
+    }
+
+    this.isFiltered = (ev) => {
+      return this.state.filters.includes(ev.category);
     }
   }
 
-  componentDidMount() {
+  onCheckboxChange(e) {
+    const filters = this.state.filters;
+    let index;
+
+    if (e.target.checked) {
+      filters.push(e.target.value);
+    } else {
+      index = filters.indexOf(e.target.value);
+      filters.splice(index, 1);
+    }
+
+    this.setState({filters: filters});
+
     getEvents((events) => {
-      this.setState({events});
+      let filteredEvents = events.filter(this.isFiltered);
+      this.setState({events: filteredEvents});
     });
+
+    console.log(filters);
+  }
+
+  componentDidMount() {
+    this.onCheckboxChange();
   }
 
   render() {
-    return (
-      <div className="calendar">
-        <BigCalendar
-          popup
-          selectable
-          events={this.state.events}
-          onSelectEvent={event => alert(event.title)}
-          views={['month', 'week', 'day', 'agenda']}
-        />
-      </div>
+    const checkboxes = calendarUrls.map(cal =>
+        <div className={`checkboxGroup checkboxGroup_${cal.category}`} key={cal.url}>
+          <input id={cal.category} type="checkbox" value={cal.category} onChange={this.onCheckboxChange.bind(this)} />
+          <label htmlFor={cal.category}>
+            {cal.category}
+          </label>
+        </div>
     );
+
+    function eventColor(event, start, end, isSelected) {
+      return { className: `rbc-event_${event.category}` }
+    };
+
+    return (
+      <div>
+        <form className="checkboxForm">
+          {checkboxes}
+        </form>
+
+        <div className="calendar">
+          <BigCalendar
+            popup
+            selectable
+            events={this.state.events}
+            eventPropGetter={eventColor}
+            onSelectEvent={event => console.log(event.title)}
+            views={['month', 'week', 'day']}
+          />
+        </div>
+      </div>
+    )
   }
 }
 
--- src/events.js
+++ src/events.js
@@ -1,46 +1,105 @@
-// const apiKey = 'AIzaSyCBwX4Bg0NlgNkR1du8uk75a4WuXOynPp8';
-// const familyCal = 'natearnold.me_97bisjqa0rb0q6sgchfkvo0v2g%40group.calendar.google.com';
-// const adultCal = 'natearnold.me_djmdkhhlncfjps47tb5mutk7t4%40group.calendar.google.com';
-//
-// const calParams = '/events?key=' + apiKey +
-// 	'&orderBy=' + orderBy +
-// 	'&singleEvents=' + singleEvents +
-// 	'&showDeleted=' + showDeleted +
-// 	'&timeMin=' + timeMin;
-
 import request from 'superagent';
 import moment from 'moment';
 
-const CALENDAR_ID = 'natearnold.me_97bisjqa0rb0q6sgchfkvo0v2g%40group.calendar.google.com';
-const API_KEY = 'AIzaSyCBwX4Bg0NlgNkR1du8uk75a4WuXOynPp8';
-let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
+const CALENDAR_URL = 'https://www.googleapis.com/calendar/v3/calendars/natearnold.me_';
+const API_KEY = '%40group.calendar.google.com/events?key=AIzaSyCBwX4Bg0NlgNkR1du8uk75a4WuXOynPp8';
 
-export function getEvents(callback) {
-	request
-		.get(url)
-		.end((err, resp) => {
-			if (!err) {
-				const events = [];
+const ADULT_CAL  = `${CALENDAR_URL}djmdkhhlncfjps47tb5mutk7t4${API_KEY}`;
+const ATTRACTION_CAL  = `${CALENDAR_URL}l6ekgqkhfoj3sj3stn2bfv59vs${API_KEY}`;
+const CIVIC_CAL  = `${CALENDAR_URL}7b0kur0154rhuh03jid2u9ffgg${API_KEY}`;
+const EDUCATION_CAL  = `${CALENDAR_URL}jeo71grdrljgndn2v3h086tl80${API_KEY}`;
+const GENERAL_CAL  = `${CALENDAR_URL}8udjniqmpi9r81rc5hne43u9m8${API_KEY}`;
+const NETWORKING_CAL  = `${CALENDAR_URL}k73uffcthf52ghtplljtbupuqg${API_KEY}`;
+const NONPROFIT_CAL  = `${CALENDAR_URL}etf11f3l3pmuikbslq5vignfd4${API_KEY}`;
+const OUTDOORS_CAL  = `${CALENDAR_URL}ggmm23r3ki4qcj27rq8u5uhsh4${API_KEY}`;
+const YOUTH_CAL  = `${CALENDAR_URL}97bisjqa0rb0q6sgchfkvo0v2g${API_KEY}`;
 
-				JSON.parse(resp.text).items.map((event) => {
-					console.log(event);
+const calendarUrls = [
+	{
+		category: 'adult',
+		url: ADULT_CAL
+	},
+  {
+		category: 'attraction',
+		url: ATTRACTION_CAL
+	},
+	{
+		category: 'civic',
+		url: CIVIC_CAL
+	},
+	{
+		category: 'education',
+		url: EDUCATION_CAL
+	},
+	{
+		category: 'general',
+		url: GENERAL_CAL
+	},
+	{
+		category: 'networking',
+		url: NETWORKING_CAL
+	},
+	{
+		category: 'nonprofit',
+		url: NONPROFIT_CAL
+	},
+	{
+		category: 'outdoors',
+		url: OUTDOORS_CAL
+	},
+	{
+		category: 'youth',
+		url: YOUTH_CAL
+	}
+];
 
-					let sTime = event.start.date || event.start.dateTime;
-					let eTime = event.end.date || event.end.dateTime;
+let calendar = [];
 
-					let csTime = moment(sTime).toDate();
-					let ceTime = moment(eTime).toDate();
+const fetchCalendar = function(cal) {
+  return new Promise((resolve, reject) => {
+		request
+			.get(cal.url)
+			.end((err, resp) => {
+				if (!err) {
 
-					events.push({
-						start: csTime,
-						end: ceTime,
-						title: event.summary,
-						description: event.description,
-						location: event.location,
-					})
-				})
+					JSON.parse(resp.text).items.map(event => {
+						let sTime = event.start.date || event.start.dateTime;
+						let eTime = event.end.date || event.end.dateTime;
 
-				callback(events);
-			}
-		})
-};
+						let csTime = moment(sTime).toDate();
+						let ceTime = moment(eTime).toDate();
+
+						return calendar.push({
+							start: csTime,
+							end: ceTime,
+							title: event.summary,
+							description: event.description,
+							location: event.location,
+							calendar: cal.url,
+							category: cal.category
+						});
+					});
+
+					resolve();
+
+				} else {
+					reject(err.message);
+				}
+			});
+  });
+}
+
+const calendarPromises = calendarUrls.map(fetchCalendar);
+
+const getEvents = (callback) => {
+	Promise.all(calendarPromises)
+	  .then(function(results) {
+			 console.log(calendar);
+			 callback(calendar);
+	  })
+	  .catch(function(err) {
+	    console.log("Failed:", err);
+	  });
+}
+
+export { getEvents, calendarUrls };
